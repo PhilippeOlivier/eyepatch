0346bfece195d63105c4438abf721e6eb9ce9fa8
Philippe Olivier <philippe.olivier@polymtl.ca>
2022-07-18 15:57:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Eyepatch
+
+Receive mobile notifications when new episodes of your favorite TV series are released.
+
+
+## How does this work?
+
+It is assumed that new episodes of popular series will be found on The Pirate Bay (TPB). This script, then, looks for episodes on TPB and notifies you if a new episode is found from a TV series that you are tracking. 
+
+
+## Setting this up
+
+You should follow these few steps to correctly set up `Eyepatch`.
+
+### Requirements
+
+* This script definitely works on Linux, probably works on macOS, and maybe works on Windows (with some tweaks).
+* You need to have [jq](https://github.com/stedolan/jq) installed on your computer.
+* You need to install the [ntfy app](https://ntfy.sh/docs/subscribe/phone/) on your phone.
+
+### Initial setup
+
+First, you need to define the values of three variables inside `eyepatch.sh`:
+1. `ntfy_topic`: The name of the topic you will subscribe to in the ntfy app to receive the notifications. Choose a topic name that is unique to you, as anyone who knows this name can view or send notifications to this topic!
+2. `track_file`: The full path of the file containing the list of TV series to track.
+3. `log_file`: The full path of the file where the logs will be written.
+
+Second, you need to subscribe to the same topic name as `ntfy_topic`, on the [ntfy app](https://ntfy.sh/docs/subscribe/phone/) on your phone.
+
+Finally, you will want to run this script periodically (ideally once per day), either as a cron job or with a systemd service.
+
+### Contents of `track_file`
+
+The file you have assigned to the variable `track_file` should follow the following format:
+* One TV series per line.
+* On line, the name of the series, followed by the season number and the episode number you want to start tracking at.
+
+#### Examples
+
+If you want track [Only Murders in the Building](https://www.imdb.com/title/tt12851524/) **starting with S02E04**, then the matching line in `track_file` for that series should be:
+
+> Only Murders in the Building 2 4
+
+If you want to track [The Rings of Power](https://www.imdb.com/title/tt7631058/), a yet **unreleased** series (at the time of this writing), then the matching line in `track_file` should just be the series name:
+
+> The Rings of Power
+
+Take note that the way torrents are generally named on TPB, you should track `The Rings of Power` rather than `The Lord of the Rings: The Rings of Power`.
---
+++ eyepatch.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+
+
+ntfy_topic="eyepatch42" # ntfy topic to subscribe to on the mobile app
+track_file="/full/path/of/eyepatch.track"
+log_file="/full/path/of/eyepatch.log"
+
+
+tpb_names() {
+	# Finds TPB torrent names associated with the search terms.
+	# Args:
+	#   $1: Search terms.
+	# Returns:
+	#   List of names of torrents associated with the results of the search terms.
+	# Usage:
+	#   tpb_names "only murders in the building s02e04"
+	local terms_raw="$1"
+	# Putting the search terms in lowercase mitigates interference from Cloudflare.
+	local terms=$(echo "$terms_raw" | tr [:upper:] [:lower:] | tr ' ' '+')
+	local results=$(curl -s "https://pirateproxy.surf/api?url=/q.php?q={$terms}&cat=")
+	# If Cloudflare blocks the search, don't parse the results with `jq`.
+	if echo "$results" | grep -qi "cloudflare"; then
+		results="cloudflare"
+	else
+		results="$(echo $results | jq .[].name)"
+	fi
+	echo "$results"
+}
+
+
+d_to_dd() {
+	# Converts a single digit to double digit (e.g., "1" to "01").
+	# Has no effect on double digits (even, e.g., "01").
+	if [[ $1 -lt 10 ]] && [[ ${#1} -eq 1 ]]; then
+		echo "0$1"
+	else
+		echo "$1"
+	fi
+}
+
+
+make_title() {
+	# Converts a series name into a title ("only murders in the building" --> "Only Murders In The Building").
+	# Args:
+	#   $1: Series.
+	# Source of this one-liner: https://stackoverflow.com/a/1538818/1725856
+	echo "$1" | sed -e "s/\b\(.\)/\u\1/g"
+}
+
+
+episode_exists() {
+	# Checks if a new episode exists on TPB.
+	# Args:
+	#   $1: Series.
+	#   $2: Season.
+	#   $3: Episode.
+	# Returns:
+	#   True if the episode exists, false otherwise.
+	# Usage:
+	#   episode_exists "only murders in the building" "2" "4"
+	local series="$1"
+	local season="$(d_to_dd $2)"
+	local episode="$(d_to_dd $3)"
+	local names=$(tpb_names "${series} S${season}E${episode}")
+	if echo "$names" | grep -q "^\"No results returned\"$"; then
+	    false
+	elif echo "$names" | grep -qi "cloudflare"; then
+		curl -d "Error with the \"eyepatch.sh\" script (Cloudflare prevented search for \"$(make_title "${series}") S${season}E${episode}\")." ntfy.sh/${ntfy_topic}
+		false
+	else
+		# Go through all the names and parse them to mitigate false positives.
+		series="$(echo "$series" | sed -E 's/ /./g' )"
+		local expression="$series.S${season}E${episode}"
+		if echo "$names" | grep -qi "$expression"; then
+			true
+		else
+			false
+		fi
+	fi
+}
+
+
+init() {
+	echo "Script initialization"
+
+	# If the tracking file does not exist, create it.
+	if [[ ! -e "$track_file" ]]; then
+		touch "$track_file"
+	fi
+
+	# If the tracking file does not end in a newline, add a newline.
+	# Source of this one-liner: https://unix.stackexchange.com/a/31955
+	sed -i -e '$a\' "$track_file"
+	
+	# Check the validity of the format of the entries in the tracking file.
+	# Format (season and episode must be 1 or 2 digits each, and not start with 0):
+	#   series season episode
+	# Note: A series which is not yet released should have "0" for season and "0" for episode.
+	# Note: A series which has no season and episode numbers, will be fixed to "0" and "0".
+	local format='^.*( [1-9][0-9]? [1-9][0-9]?| 0 0)$'
+	while read line; do
+		if [[ ! $line =~ $format ]]; then
+			sed -i "s/$line/$line 0 0/" "$track_file"
+		fi
+	done < "$track_file"
+
+	# Remove apostrophes (torrent names typically don't have apostrophes).
+	while read line; do
+		sed -i "s/$line/$(echo "$line" | tr -d "'")/g" "$track_file"
+	done < "$track_file"
+	
+	# Make sure that TPB works correctly by checking if a popular episode can be found.
+	if ! episode_exists "game of thrones" "1" "1"; then
+		curl -d "Error with the \"eyepatch.sh\" script (TPB is probably down)." ntfy.sh/${ntfy_topic}
+		exit 1
+	fi
+}
+
+
+is_series_tracked() {
+	# Checks if a series is tracked by this script (i.e., does not have a "0 0" season-episode pair).
+	# Args:
+	#   $1: Series.
+	# Returns:
+	#   True if the series is tracked, false otherwise.
+	if grep -qiE "^$1 [1-9][0-9]? [1-9][0-9]?$" "$track_file"; then
+		true
+	else
+		false
+	fi
+}
+
+
+series_exists() {
+	# Checks if a series actually exists.
+	# Args:
+	#   $1: Series.
+	# Returns:
+	#   True if the series exists, false otherwise.
+	episode_exists "$1" "1" "1"
+}
+
+
+latest_tracked_season() {
+	# Returns the latest season of a series that has been actively tracked.
+	# Args:
+	#   $1: Series.
+	if ! is_series_tracked "$1"; then
+		echo "Error: Series \"$(make_title "$1")\" is not tracked."
+		exit 1
+	fi
+	local latest_season="$(grep -iE "^$1 [1-9][0-9]? [1-9][0-9]?$" $track_file | rev | cut -d ' ' -f2 | rev)"
+	echo "$latest_season"
+}
+
+
+latest_tracked_episode() {
+	# Returns the latest episode of a series that has been actively tracked.
+	# Args:
+	#   $1: Series.
+	if ! is_series_tracked "$1"; then
+		echo "Error: Series \"$(make_title "$1")\" is not tracked."
+		exit 1
+	fi
+	local latest_episode="$(grep -iE "^$1 [1-9][0-9]? [1-9][0-9]?$" $track_file | rev | cut -d ' ' -f1 | rev)"
+	echo "$latest_episode"
+}
+
+
+get_series_from_line() {
+	# Extract the series name from a line of `track_file`.
+	# Args:
+	#   $1: One whole line of `track_file`.
+	echo "$1" | sed -E 's/^([[:print:]]*) ([1-9][0-9]? [1-9][0-9]?|0 0)$/\1/'
+}
+
+
+update_series_status() {
+	# Update a series status.
+	# Args:
+	#   $1: Series.
+	#   $2: Season.
+	#   $3: Episode.
+	# The result will be to replace the series line in `track_file` with $1 $2 $3.
+	sed -Ei "s/^$1 ([0-9][0-9]? [0-9][0-9]?|0 0)$/$1 $2 $3/" "$track_file"
+	# Log all changes.
+	echo "[$(date +%Y-%m-%d)] $(make_title "$1") S$(d_to_dd "$2")E$(d_to_dd "$3")" >> "$log_file"
+}
+
+
+series_has_new_episode() {
+	# Checks for one next new episode of the series, and updates the tracking file accordingly.
+	# Args:
+	#   $1: Series.
+	# Return:
+	#   True if a new episode has been found, false otherwise.
+	
+	local series="$1"
+
+	# If the series is untracked, check if the first episode exists. If yes, start actively tracking
+	# the series starting at S01E01. If the series is already tracked, check for new episodes.
+	if ! series_exists "$series"; then
+		echo "Series not found"
+		false
+	elif ! is_series_tracked "$series"; then
+		# If the first episode of the series exists, start tracking the episodes.
+		if episode_exists "$series" "1" "1"; then
+			echo "New episode: S01E01"
+			update_series_status "$series" "1" "1"
+			true
+		fi
+	else
+		local season="$(latest_tracked_season "$series")"
+		local episode="$(latest_tracked_episode "$series")"
+		# Check for a new episode in the current season. If it fails, check for episode 1 of the
+		# next season.
+		if episode_exists "$series" "$season" "$(( episode + 1 ))"; then
+			echo "New episode: S$(d_to_dd "$season")E$(d_to_dd "$(( episode + 1 ))")"
+			update_series_status "$series" "$season" "$(( episode + 1 ))"
+			true
+		elif episode_exists "$series" "$(( season + 1 ))" "1"; then
+			echo "New episode: S$(d_to_dd "$(( season + 1 ))")E01"
+			update_series_status "$series" "$(( season + 1 ))" "1"
+			true
+		else
+			echo "No new episodes"
+			false
+		fi
+	fi
+}
+
+
+notify() {
+	# Sends a notification for series tracking status in `track_file`.
+	# Args:
+	#   $1: Series.
+	local series="$1"
+	local season="$(latest_tracked_season "$series")"
+	local episode="$(latest_tracked_episode "$series")"
+	curl -d "NEW: $(make_title "$series") S$(d_to_dd "$season")E$(d_to_dd "$episode")" ntfy.sh/${ntfy_topic}
+}
+
+
+main() {
+	init
+	while read line; do
+		local series=$(get_series_from_line "$line")
+		echo "Searching for new episodes for series: $(make_title "$series")"
+		while series_has_new_episode "$series"; do
+			notify "$series"
+			sleep 5 # Make sure not to spam TPB so as to not get the IP banned.
+		done
+	done < "$track_file"
+}
+
+
+main
